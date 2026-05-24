2b25856c115e6a9052e2b3c0e6f7492601e7e978
Marco Ismael German Navarro <marcoi.gernav@educa.jcyl.es>
2026-05-24 12:31:53 +0100

    Corregir carga de entorno

--- src/models/repositories/JugadorRepository.php
+++ src/models/repositories/JugadorRepository.php
@@ -16,7 +16,7 @@ class JugadorRepository extends BaseRepository {
         // 1. La conexión $db
         // 2. El nombre de la tabla 'jugadores'
         // 3. El nombre de la clase Entidad (Namespace completo)
-        parent::__construct($db, 'JUGADOR', JugadorEntity::class);
+        parent::__construct($db, "Jugador", JugadorEntity::class);
     }
 
     private function hidratar(array $d): JugadorEntity {
