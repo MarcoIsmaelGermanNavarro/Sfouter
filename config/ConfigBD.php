113e5f04c9c7c71a2f1970096f2eca4f905faaf9
Marco Ismael German Navarro <marcoi.gernav@educa.jcyl.es>
2026-05-24 12:28:32 +0100

    Corregir carga de entorno

--- config/ConfigBD.php
+++ config/ConfigBD.php
@@ -12,14 +12,16 @@ class ConfigBD{
 }*/
 class ConfigBD {
     public static $SERVER_NAME;
-    public static $SERVER_PORT_BD; // <--- FALTABA ESTA
+    public static $SERVER_PORT_BD;
+    public static $CHARSET; // <--- FALTABA ESTA
     public static $DB_NAME;
     public static $USER_BD;
     public static $PASSWORD_BD;
 
     public static function init() {
         self::$SERVER_NAME    = $_ENV['DB_HOST'] ?? 'localhost';
-        self::$SERVER_PORT_BD = $_ENV['DB_PORT'] ?? '3306'; // <--- FALTABA ESTA
+        self::$SERVER_PORT_BD = $_ENV['DB_PORT'] ?? '3306';
+        self::$CHARSET        = 'utf8mb4'; // <--- AGREGA ESTA
         self::$DB_NAME        = $_ENV['DB_NAME'] ?? 'sfouter_db';
         self::$USER_BD        = $_ENV['DB_USER'] ?? 'root';
         self::$PASSWORD_BD    = $_ENV['DB_PASS'] ?? '';
--- src/models/repositories/BaseRepository.php
+++ src/models/repositories/BaseRepository.php
@@ -15,7 +15,7 @@ abstract class BaseRepository {
     protected string  $tabla; 
     
     // Si la conexion es nula se ejecuta lo que hay abajo
-    public function __construct(?PDO $db = null, String $tabla, string $entidad) {
+    public function __construct(String $tabla, string $entidad, ?PDO $db = null) {
     $this -> tabla = $tabla;
     $this -> entidad = $entidad; 
     // Si no me pasan conexión, la pido a mi clase ConexionSfouter
@@ -78,7 +78,7 @@ public function EliminarFila(int $id, ?int $idUsuario = null) {
 }
 
 
-    public function insertar(array $datos, int $idUsuario = null) {
+    public function insertar(array $datos, ?int $idUsuario = null) {
 
         try {
 
